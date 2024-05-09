c4330c6975c03de234c8f860d3b8ac9567abbc13
rufus173 <derrickotron5000@gmail.com>
2024-05-09 22:13:44 +0100

    added an autobind function

--- socket_manager.py
+++ socket_manager.py
@@ -1,4 +1,10 @@
 import socket
 class handler:
 	def __init__(self):
-		pass
+		self.server = socket.socket()
+	def auto_bind(self,port):#finds your ip and binds (requires port)
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.connect(("8.8.8.8", 80))
+		ip_address = s.getsockname()[0]
+		print("ip address:",ip_address)
+		self.server.bind((ip_address,port))
