ed81f5ae8ee26180c71264a9bcee4f57583e6ab2
rufus173 <derrickotron5000@gmail.com>
2024-05-09 22:41:19 +0100

    added some sample code on how to use the auto bind and listen features

--- tester.py
+++ tester.py
@@ -1,1 +1,15 @@
 import socket_manager
+import threading
+import socket
+import time
+
+def connect():
+    time.sleep(1)
+    server = socket.socket()
+    server.connect(("192.168.1.239",8033))
+
+server = socket.socket()
+handle = socket_manager.handler()
+server = handle.auto_bind(8033)
+threading.Thread(target=connect).start()
+handle.listen(1)
