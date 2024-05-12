6b660b034a143659bf1aac349cd727f228f84351
rufus173 <derrickotron5000@gmail.com>
2024-05-12 21:10:08 +0100

    restructured code so user is required to initiate the scan

--- socket_tester.py
+++ socket_tester.py
@@ -31,18 +31,16 @@ def port_scan():
             while sock:# if a socket is found on a port, it will try to create as many connections as possible
                 open_sockets.append(sock)
                 open_ports.append(i)
-                print("socket opened, searching for another on the same port")
+                print("socket opened, continuing search")
                 time.sleep(1)
                 sock = check_socket(ip_address,i)
-port_scan()
+    print("search complete")
+#port_scan()
 
 connect = True
 current_sock_num = 0
 
-if connect:
-    for server in open_sockets:
-        try:
-            print("""
+print("""
                  ____                           
      _______  __/ __/_  _______                 
     / ___/ / / / /_/ / / / ___/  ______         
@@ -55,80 +53,82 @@ if connect:
   /____/\____/_/  \__/ |__/|__/\__,_/_/   \___/                 
 
 
-socket testing terminal v1.0
-current commands include /close to close the socket and /recv to receive data and output it to the command line
+socket testing terminal v2.0
+to start, try /scan to try to open up a connection
+      
 input commands with / and anything else will be encoded and sent
-                  
+current commands include:
+/close to close the socket
+/recv to receive data and output it to the command line                  
 the /ls command can be used to list current open sockets and in conjunction with the /switch to change active sockets, addressed by their number
-                  
-the /rescan command will scan the ports again to try and establish more connections
-                  
+the /scan command will scan the ports again to try and establish more connections
 the /sendall is for socket_manager modules recvall function
-                  
 the /connect <port> <ip> allows you to create another connection. if ip is not specified, the local one is use instead""")
-            while True:
-                cmd = input(">>>")
-                if cmd[0] == "/":
-                    cmd = cmd[1:].split(" ")
-                    match cmd[0]:
-                        case "close":
-                            server.close()
-                        case "recv":
-                            try:
-                                server.settimeout(5.0)
-                                print(server.recv(4096))
-                            except Exception as problem:
-                                print(problem)
-                        case "ls":
-                            count = 0
-                            for i in open_sockets:
-                                print("socket number",count,"info:",i.getsockname()[0],i.getsockname()[1])
-                                count += 1
-                        case "switch":
-                            if len(cmd) == 2:
-                                selected_socket = int(cmd[1])
-                                if 0 <= selected_socket < len(open_sockets):
-                                    print("switching to",selected_socket)
+
+if connect:    
+    while True:
+        try:
+            cmd = input(">>>")
+            if cmd[0] == "/":
+                cmd = cmd[1:].split(" ")
+                match cmd[0]:
+                    case "close":
+                        server.close()
+                    case "recv":
+                        try:
+                            server.settimeout(5.0)
+                            print(server.recv(4096))
+                        except Exception as problem:
+                            print(problem)
+                    case "ls":
+                        count = 0
+                        for i in open_sockets:
+                            print("socket number",count,"info:",i.getsockname()[0],i.getsockname()[1])
+                            count += 1
+                    case "switch":
+                        if len(cmd) == 2:
+                            selected_socket = int(cmd[1])
+                            if 0 <= selected_socket < len(open_sockets):
+                                print("switching to",selected_socket)
+                                open_sockets[current_sock_num] = server #store the current socket back in open_sockets
+                                current_sock_num = selected_socket #update the selectec socket
+                                server = open_sockets[selected_socket] #set server to the new current socket
+                            else:
+                                print("couldnt perform that operation.")
+                        else:
+                            while True:
+                                try:
+                                    selected_socket = int(input("select the socket number to switch to:"))
+                                    if selected_socket == -1:
+                                        break
                                     open_sockets[current_sock_num] = server #store the current socket back in open_sockets
                                     current_sock_num = selected_socket #update the selectec socket
                                     server = open_sockets[selected_socket] #set server to the new current socket
-                                else:
-                                    print("couldnt perform that operation.")
-                            else:
-                                while True:
-                                    try:
-                                        selected_socket = int(input("select the socket number to switch to:"))
-                                        if selected_socket == -1:
-                                            break
-                                        open_sockets[current_sock_num] = server #store the current socket back in open_sockets
-                                        current_sock_num = selected_socket #update the selectec socket
-                                        server = open_sockets[selected_socket] #set server to the new current socket
-                                        break
-                                    except:
-                                        print("couldnt perform that operation. to cancel input -1")
-                        case "rescan":
-                            port_scan()
-                        case "sendall":#for the recvall of socket_manager module
-                            if len(cmd) == 1:#arguments or not
-                                server.sendall((input("enter message >>>")+"\0").encode())
-                            else:
-                                server.sendall((cmd[1]+"\0").encode())#endstop character
-                        case "connect":
-                            try:
-                                if len(cmd) == 2:
-                                    connecting_ip = ip_address
-                                else: 
-                                    connecting_ip = cmd[2]
-                                connecting_port = cmd[1]
-                                new_sock = socket.socket()
-                                new_sock.connect((connecting_ip,int(connecting_port)))
-                                open_sockets.append(new_sock)
-                                print("successfully connected!")
-                            except Exception as problem:
-                                print(problem)
-                else:
-                    server.sendall(cmd.encode())
+                                    break
+                                except:
+                                    print("couldnt perform that operation. to cancel input -1")
+                    case "scan":
+                        port_scan()
+                        server = open_sockets[-1]
+                    case "sendall":#for the recvall of socket_manager module
+                        if len(cmd) == 1:#arguments or not
+                            server.sendall((input("enter message >>>")+"\0").encode())
+                        else:
+                            server.sendall((cmd[1]+"\0").encode())#endstop character
+                    case "connect":
+                        try:
+                            if len(cmd) == 2:
+                                connecting_ip = ip_address
+                            else: 
+                                connecting_ip = cmd[2]
+                            connecting_port = cmd[1]
+                            new_sock = socket.socket()
+                            new_sock.connect((connecting_ip,int(connecting_port)))
+                            open_sockets.append(new_sock)
+                            print("successfully connected!")
+                        except Exception as problem:
+                            print(problem)
+            else:
+                server.sendall(cmd.encode())
         except Exception as problem:
-            print("error connecting with port",server.getsockname()[1],problem)
-            continue
-        current_sock_num += 1
+            print(problem)
