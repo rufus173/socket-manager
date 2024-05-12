4ad500d51358b129a1022e6ede30d463aa98adfe
rufus173 <derrickotron5000@gmail.com>
2024-05-12 20:52:52 +0100

    added some reliability features and the ability to use listen multiple times

--- socket_manager.py
+++ socket_manager.py
@@ -25,26 +25,35 @@ class handler:# the idea is that the handler stores and controlls the sockets fo
 	def listen(self,amount):# this will listen for a specific number of connections, or if -1 is given, infinite untill stop_listen is called
 		self.listening = True
 		count = 0
+		existing_count = len(self.sockets) #so we dont overwrite sockets when listening again
 		while self.listening:
-			self.server.listen(1)
-			print("listening")
-			con, address = self.server.accept()
-			print("accepted connection")
-			self.sockets[count] = con #stored as the noumber of the order they connected in (index at 0)
-			count += 1
-			if count >= amount:
-				print("finnishing search")
-				self.server.close()#stops connections leaking through
-				break
+			try:
+				self.server.listen(1)
+				print("listening")
+				con, address = self.server.accept()
+				print("accepted connection")
+				self.sockets[count+existing_count] = con #stored as the noumber of the order they connected in (index at 0)
+				count += 1
+				if count >= amount:
+					print("finnishing search")
+					self.server.close()#stops connections leaking through
+					break
+			except Exception as problem:
+				self.auto_bind(self.saved_port)
 	def stop_listen(self):#stops the listener, meant for when unlimited connections are wanted
 		self.listening = False
 	def auto_bind(self,port):#finds your ip and binds (requires port)
+		self.saved_port = port
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		s.connect(("8.8.8.8", 80))
 		ip_address = s.getsockname()[0]
 		s.close()
 		print("ip address:",ip_address)
-		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		try:
+			self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		except:
+			self.server = socket.socket()#in case it was closed before
+			self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.server.bind((ip_address,port))
 		return self.server #returns the socket if you want it, allowing flexibility if you want to manage the socket from here on out
 	def recvall(self,server):#designed to receive every last byte of data when sending large amounts reliably
